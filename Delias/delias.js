48df929044ede07457bd43b693b99059e540c213
Shannon Hartin <svhartin@gmail.com>
2018-05-28 22:37:59 -0400

    Add bootstrap menu to delias/index.html

--- Delias/delias.js
+++ Delias/delias.js
@@ -1,7 +1,13 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//     event.preventDefault();
+//
+//     $('html, body').animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 500);
+// });
+<script>
+    lightbox.option({
+      'maxWidth': 500,
+      'maxHeight':500
+    })
+</script>
